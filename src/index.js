b8b551208f2e90358de8d8b72155303a9ef3a542
Arthur <arthur.albert990@gmail.com>
2022-10-26 20:15:02 -0300

    update structure and style.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import css from "./style.css";
 
-console.log('main.js [ok]')
+console.log('index.js [ok]');
+
+let categoryName
+
+//functions
+function selectList(e){
+    categoryName = (e.target.textContent.toLowerCase());
+    console.log(categoryName)
+
+    switch (categoryName) {
+        case 'today': 
+            console.log('Load todo list for today')
+            break;
+        case 'upcoming': 
+            console.log('Load todo list for upcoming')
+            break;
+        case 'someday': 
+            console.log('Load todo list for someday')
+            break;
+        case 'anytime': 
+            console.log('Load todo list for anytime')
+            break;
+        default:
+            break;
+    }
+}
+
+//start to listen to buttons click
+//elements
+const nav = document.querySelectorAll('.category')
+
+//listeners
+
+for(let element of nav){
+    element.addEventListener('click',selectList);
+}
