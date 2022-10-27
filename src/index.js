d57c7f8d9b5e10bcdbd073f487dcfe0093ce3c91
Arthur <arthur.albert990@gmail.com>
2022-10-27 19:37:51 -0300

    update structure, style, add modal, add modalFunctions module.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import css from "./style.css";
+import * as modalFunctions from "./modalFunctions.js"
 
 console.log('index.js [ok]');
 
@@ -7,32 +8,43 @@ let categoryName
 //functions
 function selectList(e){
     categoryName = (e.target.textContent.toLowerCase());
-    console.log(categoryName)
+    console.log(categoryName);
 
     switch (categoryName) {
         case 'today': 
-            console.log('Load todo list for today')
+            console.log('Load todo list for today');
             break;
         case 'upcoming': 
-            console.log('Load todo list for upcoming')
+            console.log('Load todo list for upcoming');
             break;
         case 'someday': 
-            console.log('Load todo list for someday')
+            console.log('Load todo list for someday');
             break;
         case 'anytime': 
-            console.log('Load todo list for anytime')
+            console.log('Load todo list for anytime');
             break;
         default:
             break;
     }
 }
 
+function addTodo() {
+    console.log('addTodo, open modal');
+}
+
 //start to listen to buttons click
 //elements
-const nav = document.querySelectorAll('.category')
+const nav = document.querySelectorAll('.category');
+const addTodoBtn = document.querySelector('.btnFloat');
+const modalCloseBtn = document.querySelector('.closeBtn');
+const modalConfirmBtn = document.querySelector('.confirm');
+
 
 //listeners
-
 for(let element of nav){
     element.addEventListener('click',selectList);
 }
+
+addTodoBtn.addEventListener('click',addTodo);
+modalCloseBtn.addEventListener('click',modalFunctions.modalClose);
+modalConfirmBtn.addEventListener('click',modalFunctions.modalConfirm);
---
+++ src/modalFunctions.js
@@ -0,0 +1,9 @@
+console.log('modalFunctions')
+
+export function modalClose(){
+    console.log('close modal')
+}
+
+export function modalConfirm(){
+    console.log('confirm modal')
+}
